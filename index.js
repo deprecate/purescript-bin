6aab3488c3948fcd92b81bf1cd5d30ce40689b45
Pascal Hartig <phartig@twitter.com>
2015-03-23 14:46:46 +0000

    Fix version resolution

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 'use strict';
 
+var VERSION = module.exports.VERSION = '0.6.9.3';
+
 [
     'psc',
     'psci',
     'psc-docs',
     'psc-make'
 ].forEach(function (bin) {
-    module.exports[bin] = require('./lib')(bin).path();
+    module.exports[bin] = require('./lib')(VERSION, bin).path();
 });
